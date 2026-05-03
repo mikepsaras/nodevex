528a5fb074c573c250c6ae5a74c6ceab9a6b6f8c
Mike Psaras <me@mkps.me>
2026-05-03 15:56:20 +0300

    chore: initial project setup

---
+++ nodevex/nodevexApp.swift
@@ -0,0 +1,17 @@
+//
+//  nodevexApp.swift
+//  nodevex
+//
+//  Created by Mike on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct nodevexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
